1f7544bbe64be4bdb1c418247dd9d426fc65392c
Ahmet Can Akdaş <acanakdas@gmail.com>
2022-08-12 00:53:35 +0300

    IdentityServer Endpoints

---
+++ IdentityServer/UdemyClone.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UdemyClone.IdentityServer.DTOs;
+using UdemyClone.IdentityServer.Models;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace UdemyClone.IdentityServer.Controllers
+{
+    [Authorize(LocalApi.PolicyName)]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController:ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDto signUpDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signUpDto.Username,
+                Email = signUpDto.Email,
+            };
+            var result = await _userManager.CreateAsync(user, signUpDto.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors.Select(x => x.Description).ToList());
+            }
+            return Ok();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var users = _userManager.Users.ToList();
+            return Ok(users);
+        }
+    }
+}
+
---
+++ IdentityServer/UdemyClone.IdentityServer/DTOs/SignUpDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace UdemyClone.IdentityServer.DTOs
+{
+    public class SignUpDto
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
+
